5cd88f2b4297b9019bc30a0a5072bcd52caec3e5
oo222bs <ozan_bjk93@hotmail.com>
2020-11-26 17:27:49 +0100

    Flattening and reshaping added to CAE

--- src/cae.py
+++ src/cae.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 from config import TrainConfig
 import os, time
-fc = tf.contrib.layers.fully_connected
+
 
 # Read image and turn it into 120x160
 def read_input_folder(images_path):
@@ -42,30 +42,72 @@ def read_input_folder(images_path):
 # Convolution
 def conv_encoder(x, scope="conv_encoder"):
     with tf.variable_scope(scope, reuse=False):
-        first_conv = tf.layers.conv2d(x, 8, 4, 2, padding='same')
-        second_conv = tf.layers.conv2d(first_conv, 16, 4, 2, padding='same')
-        third_conv = tf.layers.conv2d(second_conv, 32, 4, 2, padding='same')
-        fourth_conv = tf.layers.conv2d(third_conv, 64, 8, 5, padding='same')
+        first_conv = tf.layers.conv2d(x, 8, 4, 2, padding='same', name="enc_conv1")
+        second_conv = tf.layers.conv2d(first_conv, 16, 4, 2, padding='same', name="enc_conv2")
+        third_conv = tf.layers.conv2d(second_conv, 32, 4, 2, padding='same', name="enc_conv3")
+        fourth_conv = tf.layers.conv2d(third_conv, 64, 8, 5, padding='same', name="enc_conv4")
     return fourth_conv
 
-# Fully connected layers
+# Fully connected layers (Bottleneck)
 def dense_layers(enc):
-    first_dense = fc(enc, 384, activation_fn=None)
-    second_dense = fc(first_dense, 192, activation_fn=None)
-    third_dense = fc(second_dense, 10, activation_fn=None)
-    fourth_dense = fc(third_dense, 192, activation_fn=None)
-    fifth_dense = fc(fourth_dense, 384, activation_fn=None)
+    flattened = tf.layers.flatten(enc)    # Flatten it before processing it through fully connected layers
+    first_dense = tf.layers.dense(flattened, 384, activation=None, name="bneck_fc1")
+    second_dense = tf.layers.dense(first_dense, 192, activation=None, name="bneck_fc2")
+    third_dense = tf.layers.dense(second_dense, 10, activation=None,  name="bneck_fc3")     # Visual features with 10 dimensions (will be used later)
+    fourth_dense = tf.layers.dense(third_dense, 192, activation=None, name="bneck_fc4")
+    fifth_dense = tf.layers.dense(fourth_dense, 384, activation=None, name="bneck_fc5")
     return third_dense, fifth_dense
 
 # Deconvolution
 def deconv_decoder(dense, scope="deconv_decoder"):
     with tf.variable_scope(scope, reuse=False):
-        first_deconv = tf.layers.conv2d_transpose(dense, 32, 8, 5, padding='same')
-        second_deconv = tf.layers.conv2d_transpose(first_deconv, 16, 4, 2, padding='same')
-        third_deconv = tf.layers.conv2d_transpose(second_deconv, 8, 4, 2, padding='same')
-        output = tf.layers.conv2d_transpose(third_deconv, 3, 4, 2, padding='same')
+        reshaped = tf.reshape(dense, (dense.shape[0], 3, 4, -1))     # Reshape it back to 3D before deconvolution
+        first_deconv = tf.layers.conv2d_transpose(reshaped, 32, 8, 5, padding='same', name="dec_conv1")
+        second_deconv = tf.layers.conv2d_transpose(first_deconv, 16, 4, 2, padding='same', name="dec_conv2")
+        third_deconv = tf.layers.conv2d_transpose(second_deconv, 8, 4, 2, padding='same', name="dec_conv3")
+        output = tf.layers.conv2d_transpose(third_deconv, 3, 4, 2, padding='same', name="dec_conv4_output")
     return output
 
+def extract_visual_features():
+    # get the training configuration
+    train_conf = TrainConfig()
+    train_conf.set_conf("../train/train_conf.txt")
+
+    im_data_dir = "../target/image_train"
+    resized_input = read_input_folder(im_data_dir)
+    batchsize = 1
+    placeholder = tf.placeholder(tf.float32, [resized_input.shape[1], resized_input.shape[2],
+                                              resized_input.shape[3], resized_input.shape[4]],
+                                 name="input_images")
+    # Model pipeline
+    convolved = conv_encoder(placeholder)     # Encoder block with convolutions
+    visual_features, _ = dense_layers(convolved)    # Bottleneck with visual features
+    # Use GPU
+    gpuConfig = tf.ConfigProto(
+        gpu_options=tf.GPUOptions(allow_growth=True,
+                                  per_process_gpu_memory_fraction=0.4),
+        device_count={'GPU': 1})
+
+    sess = tf.Session(config=gpuConfig)        # Launch the graph in a session
+    sess.run(tf.global_variables_initializer())        # run the session
+    #saver = tf.train.Saver(tf.global_variables())      # create a saver for the model
+    #saver.restore(sess, train_conf.cae_save_dir)         # restore previously saved variables
+
+    # Feed the dataset as input
+    for i in range(resized_input.shape[0]):
+        feed_dict = {placeholder: resized_input[i, :, :, :, :]}
+        result = sess.run(visual_features, feed_dict=feed_dict)    # run the session
+        name = "../train/visual_feature_extraction/171107/target"+'%06d' % i + ".txt"
+        dir_hierarchy = name.split("/")
+        dir_hierarchy = filter(lambda z: z != "..", dir_hierarchy)
+        save_name = os.path.join(*dir_hierarchy)
+        #dirname = '../train/' + dirname
+        save_name = os.path.join("..", save_name)
+        if not os.path.exists(os.path.dirname(save_name)):
+            os.makedirs(os.path.dirname(save_name))
+        np.savetxt(save_name, result, fmt="%.6f")
+
+        #save_latent(np.transpose(result, (1, 0, 2)), B_filenames[i], "visual_feature_extraction")  # save the predicted actions
 
 def main():
     # get the training configuration
@@ -74,7 +116,7 @@ def main():
     seed = train_conf.seed
     batchsize = 32
     epoch = train_conf.epoch
-    # set the save directory for features
+    # set the save directory for the model
     save_dir = train_conf.cae_save_dir
     if not os.path.exists(os.path.dirname(save_dir)):
         os.mkdir(os.path.dirname(save_dir))
@@ -96,15 +138,17 @@ def main():
     tf.reset_default_graph()            # Clear the default graph stack and reset the global default graph
     tf.set_random_seed(seed)            # Set the graph-level random seed
 
+    # Define a placeholder for the input
     placeholder = tf.placeholder(tf.float32, [batchsize, resized_input_batch.shape[1],
                                               resized_input_batch.shape[2], resized_input_batch.shape[3]], name="input_images")
-    convolved = conv_encoder(placeholder)
-    visual_features, dense = dense_layers(convolved)
-    output = deconv_decoder(dense)
+    # Model pipeline
+    convolved = conv_encoder(placeholder)     # Encoder block with convolutions
+    _, dense = dense_layers(convolved)    # Bottleneck with fully connected layers
+    output = deconv_decoder(dense)     # Decoder block with deconvolutions
 
     # Calculate the loss
     with tf.name_scope('loss'):
-        loss = tf.reduce_mean(tf.keras.losses.binary_crossentropy(placeholder, output))
+        loss = tf.reduce_mean(tf.keras.losses.binary_crossentropy(placeholder, output))  # Binary cross-entropy loss
 
     # Graph for update operations
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -147,4 +191,5 @@ def main():
     print(past-previous)     # print the elapsed time
 
 if __name__ == "__main__":
-    main()
+    #main()
+    extract_visual_features()
